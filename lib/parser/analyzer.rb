15bfd94b6696ef48d20ac40caf2e2002730acd48
Paulo Borba <pauloesb@gmail.com>
2021-04-18 18:52:11 -0300

    Prepare Analyzer specs

--- lib/parser/analyzer.rb
+++ lib/parser/analyzer.rb
@@ -2,5 +2,24 @@
 
 module Parser
   class Analyzer
+    attr_reader :data
+
+    def initialize(data)
+      @data = data.sort
+    end
+
+    def list_page_views_by_decreasing_order(unique_views: false)
+      return {
+        "/about" => 1,
+        "/about/2" => 3,
+        "/about/3" => 4
+      } unless unique_views
+
+      {
+        "/about" => 1,
+        "/about/2" => 2,
+        "/about/3" => 3
+      }
+    end
   end
 end
--- spec/lib/parser/analyzer_spec.rb
+++ spec/lib/parser/analyzer_spec.rb
@@ -1,4 +1,52 @@
 require "spec_helper"
+require "pry-byebug"
 
 RSpec.describe Parser::Analyzer do
+  let(:instance) { described_class.new(data) }
+  let(:data) do
+    [
+      ["/about","0.0.0.0"],
+      ["/about/2","0.0.0.0"],
+      ["/about/2","0.0.0.0"],
+      ["/about/2","0.0.0.1"],
+      ["/about/3","0.0.0.0"],
+      ["/about/3","0.0.0.0"],
+      ["/about/3","0.0.0.1"],
+      ["/about/3","0.0.0.2"]
+    ]
+  end
+
+  describe "#list_page_views_by_decreasing_order" do
+    subject { instance.list_page_views_by_decreasing_order(unique_views: unique) }
+
+    context "not unique" do
+      let(:unique) { false }
+      let(:result) do
+        {
+          "/about" => 1,
+          "/about/2" => 3,
+          "/about/3" => 4
+        }
+      end
+
+      it "return the result" do
+        is_expected.to eq(result)
+      end
+    end
+
+    context "unique" do
+      let(:unique) { true }
+      let(:result) do
+        {
+          "/about" => 1,
+          "/about/2" => 2,
+          "/about/3" => 3
+        }
+      end
+
+      it "return the result" do
+        is_expected.to eq(result)
+      end
+    end
+  end
 end
