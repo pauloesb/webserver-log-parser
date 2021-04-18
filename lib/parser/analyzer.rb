ba0e5ed5798f9ea1acdaac532e31eea3918ca54e
Paulo Borba <pauloesb@gmail.com>
2021-04-18 17:56:37 -0300

    Create analyzer structure

---
+++ lib/parser/analyzer.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Parser
+  class Analyzer
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,18 @@
-require_relative "../lib/parser/input.rb"
+require_relative "../lib/parser/input"
+require_relative "../lib/parser/analyzer"
 
 RSpec.configure do |config|
+  original_stderr = $stderr
+  original_stdout = $stdout
+  config.before(:all) do
+    $stderr = File.open(File::NULL, "w")
+    $stdout = File.open(File::NULL, "w")
+  end
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
